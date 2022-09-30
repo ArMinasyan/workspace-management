c764500bb77cd32041f9193051f84dffe26bbddd
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2022-09-30 18:57:09 +0400

    Methods return type is added

--- src/modules/Workspaces/workspaces.service.ts
+++ src/modules/Workspaces/workspaces.service.ts
@@ -1,18 +1,17 @@
-import { HttpStatus, Injectable } from "@nestjs/common";
-import { CreateWorkspaceDto } from "./dto/create-workspace.dto";
-import { UpdateWorkspaceDto } from "./dto/update-workspace.dto";
-import { InjectRepository } from "@nestjs/typeorm";
-import { WorkspaceRepository } from "./workspace.repository";
-import responseMessage from "../../common/helpers/response-message";
-import { IResponse } from "../../common/helpers/IResponse";
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { CreateWorkspaceDto } from './dto/create-workspace.dto';
+import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WorkspaceRepository } from './workspace.repository';
+import responseMessage from '../../common/helpers/response-message';
+import { IResponse } from '../../common/helpers/IResponse';
 
 @Injectable()
 export class WorkspacesService {
   constructor(
     @InjectRepository(WorkspaceRepository)
-    private readonly workspaceRepository: WorkspaceRepository
-  ) {
-  }
+    private readonly workspaceRepository: WorkspaceRepository,
+  ) {}
 
   async isSubDomainUnique(subDomain: string): Promise<boolean> {
     const workspace = await this.workspaceRepository.findBySubDomain(subDomain);
@@ -21,23 +20,23 @@ export class WorkspacesService {
 
   async create(
     userId: number,
-    payload: CreateWorkspaceDto
+    payload: CreateWorkspaceDto,
   ): Promise<IResponse> {
     const isWorkspaceDomainUnique = await this.isSubDomainUnique(
-      payload.subDomain
+      payload.subDomain,
     );
 
     if (!isWorkspaceDomainUnique) {
       return responseMessage({
         statusCode: HttpStatus.CONFLICT,
-        message: "Workspace sub domain must be a unique."
+        message: 'Workspace sub domain must be a unique.',
       });
     }
 
     const createdWorkspace = await this.workspaceRepository.createWorkspace(
       payload.name,
       payload.subDomain,
-      userId
+      userId,
     );
 
     return responseMessage({
@@ -45,22 +44,22 @@ export class WorkspacesService {
       data: {
         id: createdWorkspace.id,
         name: createdWorkspace.name,
-        subDomain: createdWorkspace.sub_domain
-      }
+        subDomain: createdWorkspace.sub_domain,
+      },
     });
   }
 
   async findAll(): Promise<IResponse> {
     const workspaces = await this.workspaceRepository.find();
     return responseMessage({
-      data: workspaces
+      data: workspaces,
     });
   }
 
   async findOne(id: number): Promise<IResponse> {
     const workspace = await this.workspaceRepository.findById(id);
     return responseMessage({
-      data: workspace
+      data: workspace,
     });
   }
 
@@ -69,32 +68,32 @@ export class WorkspacesService {
     if (!workspace?.id) {
       return responseMessage({
         statusCode: HttpStatus.NOT_FOUND,
-        message: `Workspace with id=${id} not found`
+        message: `Workspace with id=${id} not found`,
       });
     }
 
     const isWorkspaceDomainUnique = await this.isSubDomainUnique(
-      payload.subDomain
+      payload.subDomain,
     );
     if (!isWorkspaceDomainUnique) {
       return responseMessage({
         statusCode: HttpStatus.CONFLICT,
-        message: "Workspace sub domain must be a unique."
+        message: 'Workspace sub domain must be a unique.',
       });
     }
 
     await this.workspaceRepository.update(id, {
       name: payload.name,
-      sub_domain: payload.subDomain
+      sub_domain: payload.subDomain,
     });
 
     return responseMessage({
-      message: "Workspace is updated",
+      message: 'Workspace is updated',
       data: {
         id,
         name: payload.name,
-        subDomain: payload.subDomain
-      }
+        subDomain: payload.subDomain,
+      },
     });
   }
 
@@ -103,14 +102,14 @@ export class WorkspacesService {
     if (!workspace?.id) {
       return responseMessage({
         statusCode: HttpStatus.NOT_FOUND,
-        message: `Workspace with id=${id} not found`
+        message: `Workspace with id=${id} not found`,
       });
     }
 
     await this.workspaceRepository.softDelete(id);
 
     return responseMessage({
-      message: `Workspace with id=${id} deleted`
+      message: `Workspace with id=${id} deleted`,
     });
   }
 }
